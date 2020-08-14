2204b502b0840cbfc9b738efefd786b78f11f215
X.MAX <minaxu01@126.com>
2020-08-14 17:59:07 +0800

    ...

--- X.Test.AspNetCore2/Program.cs
+++ X.Test.AspNetCore2/Program.cs
@@ -16,23 +16,7 @@ namespace X.Test.AspNetCore2
     {
         public static void Main(string[] args)
         {
-            var host = CreateHostBuilder(args).Build();
-            //using (var scope = host.Services.CreateScope())
-            //{
-            //    var services = scope.ServiceProvider;
-            //    try
-            //    {
-            //        var context = services.GetRequiredService<SchoolContext>();
-            //        DbInitializer.Initialize(context);
-            //    }
-            //    catch (Exception ex)
-            //    {
-            //        var logger = services.GetRequiredService<ILogger<Program>>();
-            //        logger.LogError(ex, "An error occurred while seeding the database.");
-            //    }
-            //}
-
-            host.Run();
+            var host = CreateHostBuilder(args).Build().Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
